5c810ed04fae708c9315ea036e47d9d183cfbd0d
Robin <rtmbaertschi007@gmail.com>
2021-11-19 11:56:51 +0100

    Initial Commit

---
+++ TicTacToe/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TicTacToe
+//
+//  Created by Robin Bärtschi on 19.11.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
